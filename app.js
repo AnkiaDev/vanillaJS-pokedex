68e5b268e232c3626c80b9153b917a72131ec8b5
AnkiaDev <ankiadev@gmail.com>
2019-10-15 10:38:16 +0200

    change .then to async system, add evenlistener to card

--- app.js
+++ app.js
@@ -1,45 +1,51 @@
 const pokedex = document.getElementById("pokedex");
 
-const fetchPokemon = () => {
-  const promises = [];
-  for (let i = 1; i <= 151; i++) {
-    const url = `https://pokeapi.co/api/v2/pokemon/${i}`;
-    promises.push(fetch(url).then(res => res.json()));
-  }
-
-  Promise.all(promises).then(results => {
-    const pokemon = results.map(data => ({
-      id: data.id,
-      name: data.name,
-      img: data.sprites.front_default,
-      type: data.types.map(type => type.type.name).join(", ")
-    }));
-    displayPokemon(pokemon);
-    console.log(pokemon)
-  });
+const fetchPokemon = async () => {
+  const url = `https://pokeapi.co/api/v2/pokemon?limit=151`;
+  const res = await fetch(url);
+  const data= await res.json();
+  const pokemon = data.results.map((poke, index )=> ({
+    ...poke,
+    img: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${index + 1}.png`,
+    id: index + 1,
+  }));
+  displayPokemon(pokemon);
 };
 
 const displayPokemon = (pokemon) => {
   pokemon.map((poke)=> {
 
     const listElt = document.createElement("li");
+    listElt.classList = "card";
+    listElt.addEventListener("click", () => {
+        getPokemon(poke.id);
+    });
 
     const imgElt = document.createElement("img");
     imgElt.src = `${poke.img}`;
+    imgElt.classList = "card-img";
 
     const nameElt = document.createElement("h3");
     nameElt.textContent = `${poke.id}. ${poke.name}`;
-
-    const typeElt = document.createElement("p");
-    typeElt.textContent = `${poke.type}`;
+    nameElt.classList = "card-title";
 
     listElt.appendChild(imgElt);
     listElt.appendChild(nameElt);
-    listElt.appendChild(typeElt);
 
     pokedex.appendChild(listElt);
     
   })
 };
 
+const getPokemon = async (id) => {
+  const url = `https://pokeapi.co/api/v2/pokemon/${id}`;
+  const results = await fetch(url);
+  const pokeInfo = await results.json();
+  displayPopup(pokeInfo);
+}
+
+const displayPopup = (poke) => {
+  console.log(poke); 
+  // A terminer !!!
+}
 fetchPokemon();
