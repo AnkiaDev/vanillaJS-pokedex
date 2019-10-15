04d77ae7dbb4bfa808e2201c144f68d12ff37490
AnkiaDev <ankiadev@gmail.com>
2019-10-15 13:53:29 +0200

    add DOM manipulation for display popup when card click

--- app.js
+++ app.js
@@ -3,22 +3,22 @@ const pokedex = document.getElementById("pokedex");
 const fetchPokemon = async () => {
   const url = `https://pokeapi.co/api/v2/pokemon?limit=151`;
   const res = await fetch(url);
-  const data= await res.json();
-  const pokemon = data.results.map((poke, index )=> ({
+  const data = await res.json();
+  const pokemon = data.results.map((poke, index) => ({
     ...poke,
-    img: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${index + 1}.png`,
-    id: index + 1,
+    img: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${index +
+      1}.png`,
+    id: index + 1
   }));
   displayPokemon(pokemon);
 };
 
-const displayPokemon = (pokemon) => {
-  pokemon.map((poke)=> {
-
+const displayPokemon = pokemon => {
+  pokemon.map(poke => {
     const listElt = document.createElement("li");
     listElt.classList = "card";
     listElt.addEventListener("click", () => {
-        getPokemon(poke.id);
+      getPokemon(poke.id);
     });
 
     const imgElt = document.createElement("img");
@@ -33,19 +33,49 @@ const displayPokemon = (pokemon) => {
     listElt.appendChild(nameElt);
 
     pokedex.appendChild(listElt);
-    
-  })
+  });
 };
 
-const getPokemon = async (id) => {
+const getPokemon = async id => {
   const url = `https://pokeapi.co/api/v2/pokemon/${id}`;
   const results = await fetch(url);
   const pokeInfo = await results.json();
   displayPopup(pokeInfo);
+};
+
+const displayPopup = poke => {
+  const type = poke.types.map(type => type.type.name).join(", ");
+  const img = poke.sprites.front_default;
+  const popupDiv = document.createElement("div");
+  popupDiv.setAttribute("id", "popup");
+  const closeBtn = document.createElement("button");
+  closeBtn.setAttribute("id", "closeBtn");
+  closeBtn.textContent = "Close";
+  closeBtn.addEventListener("click", () => {
+    closePopup();
+  });
+  const classCard = document.createElement("div");
+  classCard.setAttribute("class", "card");
+  const imgElt = document.createElement("img");
+  imgElt.src = `${img}`;
+  imgElt.classList = "card-img";
+  const nameElt = document.createElement("h3");
+  nameElt.textContent = `${poke.id}. ${poke.name}`;
+  nameElt.classList = "card-title";
+  const infoElt = document.createElement("p");
+  infoElt.textContent = `Height: ${poke.height} | weight: ${poke.weight} | type: ${type}`;
+  classCard.appendChild(imgElt);
+  classCard.appendChild(nameElt);
+  classCard.appendChild(infoElt);
+  classCard.appendChild(closeBtn);
+  popupDiv.appendChild(classCard);
+  pokedex.innerHTML = "";
+  pokedex.appendChild(popupDiv);
+};
+
+const closePopup = () => {
+  pokedex.innerHTML = "";
+  fetchPokemon();
 }
 
-const displayPopup = (poke) => {
-  console.log(poke); 
-  // A terminer !!!
-}
 fetchPokemon();
